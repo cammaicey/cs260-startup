baee00763ca5296bbde2b4f17180a2e3487ca9d1
Camrynn Helliwell <chelliwell.13@gmail.com>
2023-10-24 15:19:27 -0600

    lists of all of the details needed

---
+++ char-dice.js
@@ -0,0 +1,22 @@
+var baseRaces = [
+    'Dwarf', 'Elf', 'Halfling',
+    'Human', 'Dragonborn', 'Gnome',
+    'Half-Elf', 'Half-Orc', 'Tiefling'
+];
+var baseClasses = [
+    'Barbarian', 'Bard', 'Cleric', 
+    'Druid', 'Fighter', 'Monk', 
+    'Paladin', 'Ranger', 'Rogue', 
+    'Sorcerer', 'Warlock', 'Wizard'
+];
+var alignments = [
+    'Lawfule Good', 'Neutral Good', 'Chaotic Good',
+    'Lawful Neutral', 'True Neutral', 'Chaotic Neutral',
+    'Lawful Evil', 'Neutral Evil', 'Chaotic Evil'
+];
+var baseBackgrounds = [
+    'Acolyte', 'Charlatan', 'Criminal',
+    'Entertainer', 'Folk Hero', 'Guild Artisan',
+    'Hermit', 'Noble', 'Outlander', 'Sage', 'Sailor',
+    'Soldier', 'Urchin'
+]
