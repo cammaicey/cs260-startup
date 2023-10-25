9d98eb91088fb2b08ba41b1623ea4d99afc966d9
Camrynn Helliwell <chelliwell.13@gmail.com>
2023-10-25 17:11:48 -0600

    total and rolls left change

--- sdice.js
+++ sdice.js
@@ -1,31 +1,28 @@
 const nums = [1, 2, 3, 4, 5, 6];
 let s_clicks = 6;
+let total = 0;
+let lowest = 6;
 
 function generateRoll() {
-    let rolls = [];
-    let total = 0;
-    let tdisplay = document.getElementById("total");
-    let rleft = document.getElementById("rolls-left")
+    let tdisplay = document.querySelector("#total");
+    let rleft = document.querySelector("#rolls-left")
     let table = document.getElementById("statT");
     if (s_clicks != 0) {
         s_clicks -= 1;
-        rleft = "Rolls Left: " + s_clicks.toString();
+        rleft.innerHTML = "Rolls Left: " + s_clicks.toString();
         for (let i = 0; i < 4; i++) {
             tdata = table.rows[0].cells[i];
             let roll_num = nums[Math.floor(Math.random() * nums.length)]; // will get random val from nums
             tdata.innerHTML = roll_num;
-            rolls.push(roll_num);
+            total += roll_num;
+            if (roll_num < lowest) {
+                lowest = roll_num;
+            }
+            if (i == 3) {
+                total -= lowest;
+            }
         } 
-        let low = rolls[0];
-        for (let i = 0; i < 4; i++) { // access all rolls
-            total += rolls[i]; // add rolls together
-            if (rolls[i] < low) { // keep track of the lowest roll
-                low = rolls[i];
-            }
-            if (i == 3) { // end of rolls?
-                total -= low; // subtract lowest roll
-            }
-        }
-        tdisplay = "Total: " + total;
+        tdisplay.innerHTML = "Total: " + total;
+        total = 0;
     }
 }
