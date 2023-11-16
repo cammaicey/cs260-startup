6d8b023581c89aaac4f83d0a07e33b21ccfd3afa
Camrynn Helliwell <chelliwell.13@gmail.com>
2023-11-15 17:17:50 -0700

    base code

--- database.js
+++ database.js
@@ -1,2 +1,23 @@
+const { MongoClient } = require('mongodb');
 const config = require('./dbConfig.json');
+
 const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
+const client = new MongoClient(url);
+const db = client.db('startup');
+const characterCollection = db.collection('character');
+
+// This will asynchronously test the connection and exit the process if it fails
+(async function testConnection() {
+  await client.connect();
+  await db.command({ ping: 1 });
+})().catch((ex) => {
+  console.log(`Unable to connect to database with ${url} because ${ex.message}`);
+  process.exit(1);
+});
+
+async function addCharacter(character) {
+  const result = await characterCollection.insertOne(character);
+  return result;
+}
+
+module.exports = { addCharacter };
