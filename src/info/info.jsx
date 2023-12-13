c462284a7e3672054de7ccd6aa02493584f5d1e1
Camrynn Helliwell <chelliwell.13@gmail.com>
2023-12-13 16:57:41 -0700

    jsx files made, accidentlly put home folder into info

---
+++ src/info/info.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Info() {
+  return (
+    <main className='container-fluid bg-secondary text-center'>
+      <div>Info displayed here</div>
+    </main>
+  );
+}
