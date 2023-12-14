f2512fc9177d0ebc0a1f52eb7016b30edf19f432
Camrynn Helliwell <chelliwell.13@gmail.com>
2023-12-14 12:02:40 -0700

    forgot to add authState and added a missing paraentheses from import

--- src/app.jsx
+++ src/app.jsx
@@ -4,7 +4,7 @@ import { Login } from './login/login';
 import { Home } from './home/home';
 import { Archive } from './archive/archive';
 import { Info } from './info/info';
-import { Chat } from '.chat/chat';
+import { Chat } from './chat/chat';
 import { AuthState } from './login/authState';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
