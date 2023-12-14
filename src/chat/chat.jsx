1963f8adbd9816527bdab0abb46068af3d6ac949
Camrynn Helliwell <chelliwell.13@gmail.com>
2023-12-14 11:51:48 -0700

    hopefully chat jsx works

--- src/chat/chat.jsx
+++ src/chat/chat.jsx
@@ -1,9 +1,80 @@
 import React from 'react';
+import './msg.css';
 
 export function Chat() {
+    // Adjust the webSocket protocol to what is being used for HTTP
+  const protocol = window.location.protocol === 'http:' ? 'ws' : 'wss';
+  const socket = new WebSocket(`${protocol}://${window.location.host}/ws`);
+
+  // Display that we have opened the webSocket
+  socket.onopen = (event) => {
+    appendMsg('system', 'chat', 'connected');
+  };
+
+  // Display messages we receive from our friends
+  socket.onmessage = async (event) => {
+    const text = await event.data.text();
+    const chat = JSON.parse(text);
+    appendMsg('friend', chat.name, chat.msg);
+  };
+
+  // If the webSocket is closed then disable the interface
+  socket.onclose = (event) => {
+    appendMsg('system', 'chat', 'disconnected');
+    document.querySelector('#name-controls').disabled = true;
+    document.querySelector('#chat-controls').disabled = true;
+  };
+
+  // Send a message over the webSocket
+  function sendMessage() {
+    const msgEl = document.querySelector('#new-msg');
+    const msg = msgEl.value;
+    if (!!msg) {
+      appendMsg('me', 'me', msg);
+      const name = document.querySelector('#my-name').value;
+      socket.send(`{"name":"${name}", "msg":"${msg}"}`);
+      msgEl.value = '';
+    }
+  }
+
+  // Create one long list of messages
+  function appendMsg(cls, from, msg) {
+    const chatText = document.querySelector('#chat-text');
+    chatText.innerHTML =
+      `<div><span class="${cls}">${from}</span>: ${msg}</div>` +
+      chatText.innerHTML;
+  }
+
+  // Send message on enter keystroke
+  const input = document.querySelector('#new-msg');
+  input.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') {
+      sendMessage();
+    }
+  });
+
+  // Disable chat if no name provided
+  const chatControls = document.querySelector('#chat-controls');
+  const myName = document.querySelector('#my-name');
+  myName.addEventListener('keyup', (e) => {
+    chatControls.disabled = myName.value === '';
+  });
+
   return (
-    <main className='container-fluid bg-secondary text-center'>
-      <div>chat displayed here</div>
-    </main>
+    <main>
+            <div class="name">
+            <fieldset id="name-controls">
+                <legend>My Name</legend>
+                <input id="my-name" type="text" />
+            </fieldset>
+            </div>
+
+            <fieldset id="chat-controls" disabled>
+            <legend>Chat</legend>
+            <input id="new-msg" type="text" />
+            <button onclick="sendMessage()">Send</button>
+            </fieldset>
+            <div id="chat-text"></div>
+        </main>
   );
 }
