53b8ef47e897a9338d920311252b2214bbd52f73
Camrynn Helliwell <chelliwell.13@gmail.com>
2023-12-13 17:19:16 -0700

    hopfully login is done

--- src/app.jsx
+++ src/app.jsx
@@ -1,38 +1,91 @@
 import React from 'react';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login';
+import { Home } from './home/home';
+import { Archive } from './archive/archive';
+import { Info } from './info/info';
+import { Chat } from '.chat/chat';
+import { AuthState } from './login/authState';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
-export default function App() {
+function App() {
+  const [userName, setUserName] = React.useState(localStorage.getItem('userName') || '');
+  const currentAuthState = userName ? AuthState.Authenticated : AuthState.Unauthenticated;
+  const [authState, setAuthState] = React.useState(currentAuthState);
+
   return (
-    <div className='body'>
-      <header className='container-fluid'>
-        <nav className="navbar fixed-top nav-light">
-        <a class="navbar-brand" href="#">RC</a>
-          <menu className='navbar-nav'>
-            <li className='nav-item'>
-              <a className='nav-link' href='home.html'>Home</a>
-            </li>
-            <li className='nav-item'>
-              <a className='nav-link' href='archive.html'>Archive</a>
-            </li>
-            <li className='nav-item'>
-              <a className='nav-link' href='info.html'>Info</a>
-            </li>
-            <li className='nav-item'>
-              <a className='nav-link' href='chat.html'>Chat</a>
-            </li>
-            <li className='nav-item'>
-              <a className='nav-link' href='index.html'>Logout</a>
-            </li>
-          </menu>
-        </nav>
-      </header>
+    <BrowserRouter>
+      <div className='body bg-dark text-light'>
+        <header className='container-fluid'>
+          <nav className='navbar fixed-top navbar-light'>
+          <a class="navbar-brand" href="#">RC</a>
+            <menu className='navbar-nav'>
+              <li className='nav-item'>
+                <NavLink className='nav-link' to=''>
+                  Home
+                </NavLink>
+              </li>
+              {authState === AuthState.Authenticated && (
+                <li className='nav-item'>
+                  <NavLink className='nav-link' to='play'>
+                    Chat
+                  </NavLink>
+                </li>
+              )}
+              {authState === AuthState.Authenticated && (
+                <li className='nav-item'>
+                  <NavLink className='nav-link' to='scores'>
+                    Archive
+                  </NavLink>
+                </li>
+              )}
+              <li className='nav-item'>
+                <NavLink className='nav-link' to='about'>
+                  Info
+                </NavLink>
+              </li>
+              <li className='nav-item'>
+                <NavLink className='nav-link' to='about'>
+                  Logout
+                </NavLink>
+              </li>
+            </menu>
+          </nav>
+        </header>
 
-      <main>App components go here</main>
+        <Routes>
+          <Route
+            path='/'
+            element={
+              <Login
+                userName={userName}
+                authState={authState}
+                onAuthChange={(userName, authState) => {
+                  setAuthState(authState);
+                  setUserName(userName);
+                }}
+              />
+            }
+            exact
+          />
+          <Route path='/home' element={<Home userName={userName} />} />
+          <Route path='/archive' element={<Archive />} />
+          <Route path='/info' element={<Info />} />
+          <Route path='/chat' element={<Chat />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
 
-      <footer>
-        <a href="https://github.com/cammaicey/startup">My Github Repo</a>
-      </footer>
-    </div>
+        <footer>
+            <a href="https://github.com/cammaicey/startup">My Github Repo</a>
+        </footer>
+      </div>
+    </BrowserRouter>
   );
 }
+
+function NotFound() {
+  return <main className='container-fluid bg-secondary text-center'>404: Return to sender. Address unknown.</main>;
+}
+
+export default App;
